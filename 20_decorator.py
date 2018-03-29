f17cd69f40c86463d8683fec7ba4908e90c8203d
ArisQ <qiaoli.pg@gmail.com>
2018-03-29 21:44:53 +0800

    删除多余的注释说明

--- 20_decorator.py
+++ 20_decorator.py
@@ -138,16 +138,3 @@ now5()
 print(now5.__name__)
 now6()
 print(now6.__name__)
-#
-# 请编写一个decorator，能在函数调用的前后打印出'begin call'和'end call'的日志。
-#
-# 再思考一下能否写出一个@log的decorator，使它既支持：
-#
-# @log
-# def f():
-#     pass
-# 又支持：
-#
-# @log('execute')
-# def f():
-#     pass
